65ea32df07aa7c5c60cca57028b6ca0f3d03739b
Aivo Toots <aivo.toots@cyber.ee>
2017-07-26 01:00:11 +0300

    Now supporting model xml labels used in Pleak PE-BMPN editor

--- src/app/editor/bpmn-sql-extension.ts
+++ src/app/editor/bpmn-sql-extension.ts
@@ -2,7 +2,7 @@ export const Custom = {
   id: `pleak-pa-bpmn`,
   name: `Pleak PA-BPMN`,
   prefix: `pleak`,
-  "uri": "http://pleak.io/",
+  uri: "http://pleak.io/",
 };
 
 export const SqlBPMNModdle = {
@@ -30,6 +30,94 @@ export const SqlBPMNModdle = {
           "isAttr": false,
           "type": "String"
         }
+      ]
+    },
+    {
+      name: "StereotypeTask",
+      extends: [
+        "bpmn:Task"
+      ],
+      properties: [
+        {
+          "name": "PKEncrypt",
+          "isAttr": false,
+          "type": "String"
+        },
+        {
+          "name": "PKDecrypt",
+          "isAttr": false,
+          "type": "String"
+        },
+        {
+          "name": "PKComputation",
+          "isAttr": false,
+          "type": "String"
+        },
+        {
+          "name": "MPC",
+          "isAttr": false,
+          "type": "String"
+        },
+        {
+          "name": "SKEncrypt",
+          "isAttr": false,
+          "type": "String"
+        },
+        {
+          "name": "SKDecrypt",
+          "isAttr": false,
+          "type": "String"
+        },
+        {
+          "name": "SKComputation",
+          "isAttr": false,
+          "type": "String"
+        },
+        {
+          "name": "SSSharing",
+          "isAttr": false,
+          "type": "String"
+        },
+        {
+          "name": "SSComputation",
+          "isAttr": false,
+          "type": "String"
+        },
+        {
+          "name": "SSReconstruction",
+          "isAttr": false,
+          "type": "String"
+        },
+        {
+          "name": "AddSSSharing",
+          "isAttr": false,
+          "type": "String"
+        },
+        {
+          "name": "AddSSComputation",
+          "isAttr": false,
+          "type": "String"
+        },
+        {
+          "name": "AddSSReconstruction",
+          "isAttr": false,
+          "type": "String"
+        },
+        {
+          "name": "FunSSSharing",
+          "isAttr": false,
+          "type": "String"
+        },
+        {
+          "name": "FunSSComputation",
+          "isAttr": false,
+          "type": "String"
+        },
+        {
+          "name": "FunSSReconstruction",
+          "isAttr": false,
+          "type": "String"
+        },
       ]
     },
     {
