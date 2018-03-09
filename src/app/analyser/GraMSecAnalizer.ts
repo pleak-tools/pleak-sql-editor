ce5276051607697216b966e027e4cfc05dd01fa3
Aivo Toots <aivo.toots@cyber.ee>
2018-03-09 11:19:02 +0200

    Results table bugfix

--- src/app/analyser/GraMSecAnalizer.ts
+++ src/app/analyser/GraMSecAnalizer.ts
@@ -279,10 +279,12 @@ export let computeSensitivitiesMatrix = (process: any, registry: any): [any, any
       });
       if (sameElements.length == 0) {
         Ds.push({input: source, output: target, value: value});
+      } else {
+        sameElements[0].value = value;
       }
 
       // Add calculated value to the result matrix
-      matrix.push({input: source, output: target, value: value})
+      matrix.push({input: source, output: target, value: value});
 
     }
   }
